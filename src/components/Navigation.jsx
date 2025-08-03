9ffb2dcccd6568f7d127fdac5f3b3efe15efb0f5
AmbiNtsoah <ambinints04@gmail.com>
2025-08-03 09:42:12 +0300

    add: Add React-Router, components and pages folder

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+/* This is the Navigation Bar component*/
+function Navigation({title}) {
+  return (
+    <>
+      <div>{title}</div>
+    </>
+  );
+}
+
+export default Navigation;
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import Navigation from "../components/Navigation";
+
+function LandingPage() {
+  return (
+    <>
+      <Navigation title='Gascom Navigation Bar here'/>
+      <p>Landing Page</p>
+    </>
+  );
+}
+
+export default LandingPage;
